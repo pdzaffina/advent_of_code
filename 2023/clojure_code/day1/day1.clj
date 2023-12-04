9a7597509c13663143076332382b87d87fc37ddd
Peter D. Zaffina <pdzaffina@gmail.com>
2023-12-04 08:21:15 -0600

    completed day 1 part 1

---
+++ 2023/clojure_code/day1/day1.clj
@@ -0,0 +1,21 @@
+(ns clojure-code.day1.day1
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+;; day1 part1
+
+;; Get the input text and convert to string
+(def input (str/split-lines (slurp "./resources/day1/input.txt")))
+
+;; Define a function to extract the first and last digits from a line and calculate the sum
+(defn sum-of-calibration-values [lines]
+  (->> lines
+       (map #(re-seq #"\d" %))                    ; Extract all digits from the text
+       (map #(Integer. (str (first %) (last %)))) ; Extract and convert to integers
+       (apply +)))                                ; Sum the values
+
+;; Calculate the sum of all calibration values
+(def result (sum-of-calibration-values input))
+
+;; Output the result
+(println result)
