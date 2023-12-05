fe7ce3429333997967f7c17100a7a62b4f26bb7e
Peter D. Zaffina <pdzaffina@gmail.com>
2023-12-04 20:45:20 -0600

    Update day1.clj for part 2

--- 2023/clojure_code/day1/day1.clj
+++ 2023/clojure_code/day1/day1.clj
@@ -2,20 +2,37 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-;; Day 1 Part 1
+(def sample-input (str/split-lines (slurp "./resources/day1/sample-input.txt")))
 
-;; Get the input text and convert to string
+(def sample-input2 (str/split-lines (slurp "./resources/day1/sample-input2.txt")))
+
 (def input (str/split-lines (slurp "./resources/day1/input.txt")))
 
-;; Define a function to extract the first and last digits from a line and calculate the sum
+; Define a function to replace written-out digits with numeric representations
+(defn replace-written-digits [s]
+  (-> s
+      (str/replace #"one" "one1one")
+      (str/replace #"two" "two2two")
+      (str/replace #"three" "three3three")
+      (str/replace #"four" "four4four")
+      (str/replace #"five" "five5five")
+      (str/replace #"six" "six6six")
+      (str/replace #"seven" "seven7seven")
+      (str/replace #"eight" "eight8eight")
+      (str/replace #"nine" "nine9nine")
+      (str/replace #"zero" "zero0zero")))
+
+
+; Define a function to extract the first and last digits from a line and calculate the sum
 (defn sum-of-calibration-values [lines]
   (->> lines
-       (map #(re-seq #"\d" %))                    ; Extract all digits from the text
+       (map replace-written-digits)
+       (map #(re-seq #"\d" %))
        (map #(Integer. (str (first %) (last %)))) ; Extract and convert to integers
        (apply +)))                                ; Sum the values
 
-;; Calculate the sum of all calibration values
+; Calculate the sum of all calibration values
 (def result (sum-of-calibration-values input))
 
-;; Output the result
+; Output the result
 (println result)
